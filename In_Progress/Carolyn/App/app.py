b752b5497392b5a253b525123e9ceacd2d342b11
carolyn-tsai <53666793+carolyn-tsai@users.noreply.github.com>
2020-04-09 13:27:10 -0700

    clean up python code by adding for loop and add html input required attribute

--- In_Progress/Carolyn/App/app.py
+++ In_Progress/Carolyn/App/app.py
@@ -21,37 +21,19 @@ def home_page():
 @app.route("/send", methods=["GET", "POST"])
 def send_data():
     if request.method == "POST":
-        age = int(request.form["age"])
-        band_size = int(request.form["band_size"])
-        height = int(request.form["height"])
-        weight = int(request.form["weight"])
-        cup_size = int(request.form["cup_size"])
-        bra_color = int(request.form["bra_color"])
-        mastectomy_side = int(request.form["mastectomy_side"])
-        lace_details = int(request.form["lace_details"])
-        shoulder_pain = int(request.form["shoulder_pain"])
-        smooth_cup = int(request.form["smooth_cup"])
-        molded_foam = int(request.form["molded_foam"])
-        wired = int(request.form["wired"])
-        lace_insert = int(request.form["lace_insert"])
-        front_back = int(request.form["front_back"])
-        active = int(request.form["active"])
-        reach_back = int(request.form["reach_back"])
-        wide_strap = int(request.form["shoulder_pain"])
-        padded_strap = int(request.form["shoulder_pain"])
-        brand = int(request.form["brand"])
-        embroidery = int(request.form["embroidery"])
-        jacquard = int(request.form["jacquard"])
-        patient_submission = [[age, weight, height, band_size, cup_size, mastectomy_side, shoulder_pain, bra_color, brand, wide_strap, padded_strap, wired, molded_foam, smooth_cup, lace_details, active, lace_insert, front_back, reach_back, embroidery, jacquard]]
+        attr_list = ["age", "weight", "height", "band_size", "cup_size", "mastectomy_side", "shoulder_pain", "bra_color", "brand", "shoulder_pain", "shoulder_pain", "wired", "molded_foam", "smooth_cup", "lace_details", "active", "lace_insert", "front_back", "reach_back", "embroidery", "jacquard"]     
+        user_sub = []
+        for i in attr_list:
+            attr = int(request.form[i])
+            user_sub.append(attr)
+        style_pred = [user_sub]
         dec_tree_model = open("decision_tree_model.sav", "rb")
         tree_model = pickle.load(dec_tree_model)
-        recommendation = tree_model.predict(patient_submission)
+        recommendation = tree_model.predict(style_pred)
         bra_recommendation = recommendation[0]
         bra_image_jpg = f"{bra_recommendation}.JPG"
         bra_image = os.path.join(app.config["bra_images"], bra_image_jpg)
         return render_template("recommendation.html", bra_style=bra_recommendation, bra_image_path=bra_image)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
